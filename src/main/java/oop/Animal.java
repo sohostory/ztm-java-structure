1bb22fc212ae5a8c3cea76ee48d2fd4ce31b35a4
Hoyoung Lee <sohostory@gmail.com>
2023-08-21 11:32:24 -0500

    oop exercise completed

---
+++ src/main/java/oop/Animal.java
@@ -0,0 +1,21 @@
+package oop;
+
+public class Animal {
+    private String sound;
+
+    public Animal (String sound) {
+        setSound(sound);
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    public void makeSound() {
+        System.out.println("Animal sound: " + getSound());
+    }
+}
